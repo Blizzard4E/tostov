a501231f3f72591eec9e7649767d0435ade16ae1
Blizzard4E <blizzard4e@gmail.com>
2025-03-30 18:43:48 +0700

    add page title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	compatibilityDate: "2024-11-01",
-	nitro: {
-		preset: "node-server",
+	app: {
+		head: {
+			title: "TosTov",
+		},
 	},
+	compatibilityDate: "2024-11-01",
 	devtools: { enabled: true },
 	css: ["~/assets/css/main.css", "~/assets/css/animations.css"],
 	modules: [
