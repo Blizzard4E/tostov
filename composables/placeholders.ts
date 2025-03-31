55d29c78037a0b76b69ce153da6e2e8676aae783
Blizzard4E <blizzard4e@gmail.com>
2025-03-31 11:34:03 +0700

    updated placeholder data's images

--- composables/placeholders.ts
+++ composables/placeholders.ts
@@ -4,7 +4,7 @@ export const placeholderDestinations: Destination[] = [
 		name: "Café Museum",
 		description:
 			"Lying on the 5th floor of U Mall, Café Museum offers trendy aesthetics and modern minimalism setting for coffee lovers seeking a quiet space to work, read, or simply unwind. With their unique menu items, signature drinks, and delicious pastries make you want to revisit and try everything.  Combine with their unique theme, Café Museum truly worth a visit.",
-		cover: "https://res.cloudinary.com/duzvevuup/image/upload/v1743393937/TosTov/tiivmxvudnl5s4oo7ocz.jpg",
+		cover: "https://res.cloudinary.com/duzvevuup/image/upload/v1743395612/museum-cafe_farqfj.jpg",
 		address: "U Mall, 5th Floor, Phnom Penh",
 		rating: 5,
 	},
@@ -13,7 +13,7 @@ export const placeholderDestinations: Destination[] = [
 		name: "May Floral&Cafe",
 		description:
 			'Near the Independence Monument, a combination of café and floral shop truly stand out from your typical conventional coffee shop comfort. Though with limited options of their beverage menu (fruit tea and matcha only) May Floral&Cafe creates a visually stunning environment perfectly "Instagrammable" and make you want to stay longer for their sweet fragrant atmosphere. Bring your love one for a visit, as you can get them a refreshing drink and a lovely set of flowers.',
-		cover: "https://res.cloudinary.com/duzvevuup/image/upload/v1743394256/TosTov/flower_duzuyk.jpg",
+		cover: "https://res.cloudinary.com/duzvevuup/image/upload/v1743330410/TosTov/flower_eccmd9.jpg",
 		address: "14 Samdach Louis Em St. (282), Phnom Penh",
 		rating: 5,
 	},
@@ -22,7 +22,7 @@ export const placeholderDestinations: Destination[] = [
 		name: "Cā Feve Coffee & Eatery",
 		description:
 			'Who knows in the Toul Tom Poung (TTP) area of Phnom Penh lies a café with stunning floral decorations. Cā Feve Coffee & Eatery offers both indoor and outdoor atmostphere, providing such a nice escape with a variety of coffee and other refreshments, sweet parties, and appetizing meals. "Cozy" and "beautiful", worthy of an insta post.',
-		cover: "https://res.cloudinary.com/duzvevuup/image/upload/v1743394390/TosTov/cafeve_pogfwr.jpg",
+		cover: "https://res.cloudinary.com/duzvevuup/image/upload/v1743330410/TosTov/cafeve_qp4heq.jpg",
 		address: "Toul Tom Poung (TTP) area, St 123",
 		rating: 5,
 	},
@@ -31,7 +31,7 @@ export const placeholderDestinations: Destination[] = [
 		name: "Weeee Wonder Coffee",
 		description:
 			"Looking for wide range of drinks? Weeee Wonder Coffee is the place for you. Their unqiue menu offers a diverse selection of drinks, including classic coffee options (Latte, Americano, Cappuccino, Mocha), as well as specialty items like Pumpkin Latte, Shaken Yuzu Espresso, and Black Sugarcane, and also a variety of Matcha drinks, fruit teas and other iced and hot beverages. Cute, modern, trendy, exlusive drinks perfect to mix up a new taste to your daily dose of caffeine. ",
-		cover: "https://res.cloudinary.com/duzvevuup/image/upload/v1743394427/TosTov/wee_rnhimr.jpg",
+		cover: "https://res.cloudinary.com/duzvevuup/image/upload/v1743330409/TosTov/wee_npg24i.jpg",
 		address: "45 Road, Room 1, Daun Penh, Phnom Penh",
 		rating: 5,
 	},
@@ -40,7 +40,7 @@ export const placeholderDestinations: Destination[] = [
 		name: "PAENG Cafe & Eatery",
 		description:
 			"More than just a place to grab a quick bite. PAENG Cafe & Eatery offers buffet meal, a wide range of Khmer, Thai, and Vietnamese cuisines within a warm and inviting atmosphere and affordable price. Located near the Royal Palace, both locals and travelers can experience a taste of local culture and flavors.  Don't miss the opportunity to try their amazing egg coffee, a highly recommended beverage.",
-		cover: "https://res.cloudinary.com/duzvevuup/image/upload/v1743394491/TosTov/paeng_abiyek.jpg",
+		cover: "https://res.cloudinary.com/duzvevuup/image/upload/v1743330409/TosTov/paeng_zzrklk.jpg",
 		address: "No.99, CORNER ST.136 AND ST, 13, Phnom Penh",
 		rating: 5,
 	},
